405713be160ea2dde7451f72a5eb048310fb63ce
manuelnelson <elnels@gmail.com>
2013-02-27 18:30:52 -0600

    Latest updates

--- FriendlyForms/Scripts/app/friendly-output.js
+++ FriendlyForms/Scripts/app/friendly-output.js
@@ -3,23 +3,5 @@
     html = html.replace(/<form.*>/, "");
     html = html.replace(/<input.*>/, "");
     html = html.replace(/<footer[^>]*?>([\s\S]*)<\/footer>/, "");
-    //html = html.replace(/<hr.*>/, "");
-    $('#html').val(html);
-    //$('.printForm').click(function() {
-    //    var html = $('#main-content').html();
-    //    //remove button at end of form
-    //    html = html.replace(/<input.*>/, "");
-    //    Friendly.StartLoading();
-    //    $.ajax({
-    //        url: '/Output/PrintForm',
-    //        type: 'POST',
-    //        data: {
-    //            html: html
-    //        },
-    //        success: function(data) {
-    //            Friendly.EndLoading();
-    //        },
-    //        error: Friendly.GenericErrorMessage
-    //    });
-    //});
+    $('.html').val(html);
 });
