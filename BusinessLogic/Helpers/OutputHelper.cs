4f32127da8ab8c0c3382af402a9663853245e7ef
Emmanuel Nelson <elnels@gmail.com>
2013-09-26 13:10:26 -0500

    Fix to total calculation

--- BusinessLogic/Helpers/OutputHelper.cs
+++ BusinessLogic/Helpers/OutputHelper.cs
@@ -6,7 +6,7 @@ namespace BusinessLogic.Helpers
     {
         public static double CalculateTotalIncome(this IncomeDto income)
         {
-            return income.W2Income + income.Commisions + income.SelfIncomeNoDeductions + income.Bonuses + income.Overtime +
+            return income.W2Income + income.Commisions + income.SelfIncome + income.Bonuses + income.Overtime +
                    income.Severance + income.Retirement + income.Interest + income.Dividends + income.Trust + income.Annuities +
                    income.Capital + income.SocialSecurity + income.Compensation + income.Unemployment + income.CivilCase + income.Gifts + income.Prizes +
                    income.Alimony + income.Assets + income.Fringe + income.Other;
